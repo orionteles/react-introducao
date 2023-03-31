ee4c8ea3266008a47ae004a2a4ed0833e9c86a23
Orion Teles de Mesquita <orionteles@gmail.com>
2023-03-30 21:56:27 -0300

    first commit

---
+++ pages/deputados.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import apiFilmes from '../services/apiFilmes'
+
+const deputados = () => {
+
+    const [deputados, setDeputados] = useState([])
+
+    useEffect(() => {
+        apiFilmes.get('/deputados').then(resultado => {
+            setDeputados(resultado.data.dados)
+        })
+    }, [])
+
+    return (
+        <>
+            <h1>deputados</h1>
+
+            <div>
+                {deputados.map(item => (
+                    <img key={item.id} src={item.urlFoto} width="100px" />
+                ))}
+            </div>
+
+
+        </>
+    )
+}
+
+export default deputados
